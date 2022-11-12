5858fe6524b0c82b30aaec0241508fc4589c7840
Dipbaviskar <106143172+Dipbaviskar@users.noreply.github.com>
2022-11-12 16:06:22 +0530

    2 files modified

--- index.tsx
+++ index.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
-
+function click_me() {
+  console.log(Math.random());
+}
+function EventHandling() {
+  return <button onClick={click_me}>Click me</button>;
+}
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(<EventHandling />);
